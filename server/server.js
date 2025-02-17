a5e111fe0740f3707d1c973bd0555cbae7c5b84a
Mahesh <maheshkolli888@gmail.com>
2025-02-18 02:48:48 +0530

    v3s

--- server/server.js
+++ server/server.js
@@ -9,18 +9,8 @@ const adminApp = require("./APIs/adminApi");
 const cors = require('cors')
 app.use(cors())
 
-
-
-const port = process.env.PORT || 4000;
-
-//db connnection
-mongoose
-  .connect(process.env.DBURL)
-  .then(() => {
-    app.listen(port, () => console.log(`Server listening on port: ${port}`));
-    console.log("DB Connection Success");
-  })
-  .catch((err) => console.log("Error in DB Connection ", err));
+// Get port from environment variable or use 3000 as fallback
+const port = process.env.PORT || 3000;
 
 //body parser middleware
 app.use(exp.json());
@@ -30,10 +20,25 @@ app.use("/user-api", userApp);
 app.use("/author-api", authorApp);
 app.use("/admin-api", adminApp);
 
-
+// Basic route to test if server is running
+app.get('/', (req, res) => {
+  res.send('VENZO API is running');
+});
 
 //error handler
 app.use((err, req, res, next)=>{
   console.log("Error object in express error handler : ", err);
   res.send({message:err.message})
 })
+
+// Connect to MongoDB first, then start the server
+mongoose
+  .connect(process.env.DBURL)
+  .then(() => {
+    // Start server after successful DB connection
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Server listening on port: ${port}`);
+    });
+    console.log("DB Connection Success");
+  })
+  .catch((err) => console.log("Error in DB Connection ", err));
